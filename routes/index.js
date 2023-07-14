60e0fa0ecba2e988dab5fb746c2bc900a18e4879
Sam Teel <sjkteel@hotmail.com>
2023-07-14 17:13:50 -0400

    changed server line 17 path

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 //import notes content
 const noteRouter = require('./notes');
-
 //use express through app variable
 const app = express();
 //routes notes to index
